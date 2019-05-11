fc17726d4d7005f0434617ca3094589ca221da4e
levinxo <levinxo@gmail.com>
2019-05-11 23:59:52 +0800

    Pow(x, n)的迭代法

--- algorithm/powx_n.go
+++ algorithm/powx_n.go
@@ -5,24 +5,52 @@ import "fmt"
 //Pow(x, n)
 //https://leetcode.com/problems/powx-n/description/
 
-//@todo 还可以用迭代来算
-
 //利用递归的分治法
 //Time complexity O(log n)
 //Space complexity O(?)
-func myPow(x float64, n int) float64 {
+func myPow1(x float64, n int) float64 {
 
     fmt.Printf("param n: %d\tparam x: %f\n", n, x)
     if n == 0 {
         return 1
     }
     if n < 0 {
-        return 1/myPow(x, -n)
+        return 1/myPow1(x, -n)
     }
     if n % 2 != 0 {
-        return x * myPow(x*x, (n-1)/2)
+        return x * myPow1(x*x, (n-1)/2)
     }
-    return myPow(x*x, n/2)
+    return myPow1(x*x, n/2)
+}
+
+//利用迭代的分治法
+//Time complexity O(log n)
+//Space complexity O(1)
+func myPow2(x float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+    if n < 0 {
+        x = 1/x
+        n = -n
+    }
+
+    var res float64
+    res = 1
+
+    for n > 0 {
+        fmt.Printf("n=%d, ", n)
+        //奇数时，这里res把n为奇数的时候多余的x给留存起来，
+        //并在最后n=1的时候乘以x最终值，再return
+        if n&1 == 1 {
+            fmt.Printf("res=%f, x=%f, ", res, x)
+            res *= x
+        }
+        fmt.Printf("x=%f\n",  x)
+        x *= x
+        n >>= 1
+    }
+    return res
 }
 
 func main(){
@@ -32,9 +60,11 @@ func main(){
     )
 
     x = 2
-    n = 4
+    n = 5
 
-    ret := myPow(x, n)
+    ret := myPow1(x, n)
+    fmt.Println(ret)
+    ret = myPow2(x, n)
     fmt.Println(ret)
 }
 
