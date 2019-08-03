6f112cf89a7d11db8acd770c29b78c6107fd6c77
levinxo <levinxo@gmail.com>
2019-08-03 18:06:41 +0800

    交换次数较少的快速排序

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -9,7 +9,7 @@ import (
  * Time Complexity O(n logn)
  * Space Complexity
  */
-func QuickSort(data []int){
+func QuickSort1(data []int){
     if len(data) < 2 {
         return
     }
@@ -27,14 +27,50 @@ func QuickSort(data []int){
             i++
         }
     }
-    QuickSort(data[:head])
-    QuickSort(data[head+1:])
+    QuickSort1(data[:head])
+    QuickSort1(data[head+1:])
+}
+
+//少交换次数的版本
+func QuickSort2(data []int, start, end int) {
+    if len(data) < 2 || start >= end {
+        return
+    }
+
+    i, j := start, end
+    pivot := data[start + (end - start) / 2]
+
+    for i <= j {
+        for data[i] < pivot {
+            i++
+        }
+        for data[j] > pivot {
+            j--
+        }
+        if i < j {
+            data[i], data[j] = data[j], data[i]
+            i++
+            j--
+        } else if i == j {
+            i++
+        }
+    }
+
+    QuickSort2(data, start, j)
+    QuickSort2(data, i, end)
 }
 
 func main() {
-    data := []int{22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, -1, 70}
+    var data []int
+
+    fmt.Println("第一种方法：")
+    data = []int{22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, -1, 70}
+    QuickSort1(data)
     fmt.Println(data)
-    QuickSort(data)
+
+    fmt.Println("第二种方法：")
+    data = []int{22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, -1, 70}
+    QuickSort2(data, 0, len(data)-1)
     fmt.Println(data)
 }
 
