eb708f898115049139e75e4fe9f3761835ac2876
levinxo <levinxo@gmail.com>
2020-05-02 15:32:09 +0800

    64.minimum-path-sum update

--- dynamic_programming/64.minimum-path-sum/Solution.java
+++ dynamic_programming/64.minimum-path-sum/Solution.java
@@ -1,9 +1,10 @@
+
 /**
  * https://leetcode.com/problems/minimum-path-sum/
  * 64. Minimum Path Sum
  * 新建一个dp数组，和原先矩阵保持一致，dp[i][j]记录从坐标i,j到右下角的最小路径值
  * 初始化右下角的dp值为对应的矩阵值，然后去填整个矩阵，每个元素考虑移动到右边或下面
- * 可得转移方程：dp[i,j] = grid[i,j] + max(dp[i+1,j], dp[i,j+1])
+ * 可得转移方程：dp[i,j] = grid[i,j] + min(dp[i+1,j], dp[i,j+1])
  * 同时要注意边界情况，最右列和最下行
  */
 
@@ -13,10 +14,13 @@ public class Solution {
 
     public static void main(String... args) {
         int[][] grid = new int[][] { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
-        System.out.println(minPathSum(grid));
+        System.out.println("第1种方法：");
+        System.out.println(minPathSum1(grid));
+        System.out.println("第2种方法：");
+        System.out.println(minPathSum2(grid));
     }
 
-    public static int minPathSum(int[][] grid) {
+    public static int minPathSum1(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
@@ -41,6 +45,33 @@ public class Solution {
         }
 
         return dp[0][0];
+    }
+
+    public static int minPathSum2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int column = grid[0].length;
+        int[][] dp = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0 && j != 0) {
+                    // 第一行，只能从左侧位置过来
+                    dp[i][j] = grid[i][j] + dp[i][j - 1];
+                } else if (j == 0 && i != 0) {
+                    // 第一列，只能从上方位置过来
+                    dp[i][j] = grid[i][j] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[row - 1][column - 1];
     }
 
 }
