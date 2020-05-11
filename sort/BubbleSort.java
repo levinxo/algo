29e102f1d8cace5a9c1cb05d544594d3cb7602b1
levinxo <levinxo@gmail.com>
2020-05-11 23:51:24 +0800

    冒泡排序

---
+++ sort/BubbleSort.java
@@ -0,0 +1,40 @@
+class BubbleSort {
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5,2,18,3,11,90,45,22};
+        arr = bubbleSort(arr);
+        for (int a: arr) {
+            System.out.println(a);
+        }
+    }
+
+
+    /**
+     * Time Complexity O(n^2)
+     * Space Complexity O(1)
+     */
+    public static int[] bubbleSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return arr;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean change = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = tmp;
+                    change = true;
+                }
+            }
+            if (!change) {
+                break;
+            }
+        }
+        return arr;
+    }
+
+
+}
