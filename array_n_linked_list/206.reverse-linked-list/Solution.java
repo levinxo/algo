cab6adb53b134689559394f09343fa9980fa5df3
levinxo <levinxo@gmail.com>
2020-05-21 23:06:10 +0800

    206. Reverse Linked List

---
+++ array_n_linked_list/206.reverse-linked-list/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * 206. Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+
+public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+
+}
