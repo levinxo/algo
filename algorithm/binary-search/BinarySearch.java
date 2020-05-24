54e88e6f67977ccc2f4a6b3b20ec101f86594666
levinxo <levinxo@gmail.com>
2020-05-24 20:44:00 +0800

    常规二分查找

---
+++ algorithm/binary-search/BinarySearch.java
@@ -0,0 +1,33 @@
+public class BinarySearch {
+
+
+    public static int binarySearch(int[] arr, int value) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        int l = 0, r = arr.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;    // 每次循环要重新计算m
+            if (arr[m] == value) {
+                return m;
+            } else if (arr[m] > value) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,7,9,11,13,15,17};
+        int value = 3;
+        int index = binarySearch(arr, value);
+        System.out.println(index);
+    }
+
+
+}
